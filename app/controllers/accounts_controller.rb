0991f4ba43472a616fa3b9cce059cfd6f4f449f4
Jenny Kam <jkam17@gmail.com>
2019-10-01 19:14:38 -0400

    add encrypted alphavantage api credentials, bug fix for stock record creation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -21,9 +21,9 @@ class AccountsController < ApplicationController
     end
     if params[:id] != current_account.id.to_s
       flash[:error]="You may not access another user's data"
+      redirect_to account_path(current_account)
     end
     @account = current_account
-    @stock = Stock.new
   end
 
   def alphavantage
@@ -32,7 +32,7 @@ class AccountsController < ApplicationController
     resp = Faraday.get "https://www.alphavantage.co/query" do |req|
       req.params["function"] = "GLOBAL_QUOTE"
       req.params["symbol"] = params[:symbol]
-      req.params["apikey"] = 'key'
+      req.params["apikey"] = Rails.application.credentials.alphav #remove key
     end
     body = JSON.parse(resp.body)
 
@@ -40,12 +40,12 @@ class AccountsController < ApplicationController
     price = stock["05. price"]
     if @account.balance.to_f > (price.to_f * params[:shares].to_f)
       s = Stock.where(account_id: @account.id).find_or_create_by(symbol: params[:symbol])
-      s.shares += params[:shares].to_i
+      s.shares = s.shares ? s.shares += params[:shares].to_i : params[:shares].to_i
       s.save
       @account.balance = @account.balance.to_f - (price.to_f * params[:shares].to_f)
       @account.save
     end
-    render :show
+    redirect_to account_path(current_account)
   end
 
   private
