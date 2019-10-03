e3773f595019f0fee84e2c9d45ad6311ab512337
Jenny Kam <jkam17@gmail.com>
2019-10-02 23:38:44 -0400

    finish transaction js load

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -16,6 +16,17 @@ function showTransactions(){
   $('.show-t').on('click', function(e){
     e.preventDefault()
     $('.page-name').html(`<span class='page-title'>Transactions</span>`)
+
+    $.get('/transactions', function(json){
+      let pagehtml = '<div>'
+      for (const el of json){
+        pagehtml += `
+          <p> BUY (${el.symbol}) - ${el.shares} @ ${el.price} ---- ${el.created_at}</p>
+           `
+      }
+      pagehtml+='</div>'
+      $('.container').html(pagehtml)
+    })
   })
 }
 
@@ -23,21 +34,24 @@ function showPortfolio(){
   $('.show-p').on('click', function(e){
     e.preventDefault
     let id = $(this).data('account')
-    $.get('/stocks', function(json){  //first column, stock data and api data
-      for (const stock of json){
-        $.post('/alphavantage', {}, function(json){
+    let pagehtml
+    $.get('/stocks', function(sjson){  //first column, stock data and api data
+      for (const stock of sjson){
+        $.post('/alphavantage',
+          {}
+          , function(apijson){
 
         })
       }
 
 
 
-      let titlehtml = `<span class='page-title'>Portfolio</span> <span id='port_value'> ( ${} )</span>`
+      // let titlehtml = `<span class='page-title'>Portfolio</span> <span id='port_value'> ( ${} )</span>`
 
       $('.page-name').html(titlehtml)
     })
     $.get('/account/'+id+'.json', function(json){ //second column, account data for form
-      let formhtml = `<div class="col">
+      pagehtml = `<div class="col">
     <p>Balance: $${json.balance}</p>
     <form action="/alphavantage" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="AOa8fXpbF/IcEy52ALnVXYWNUr1kvaF5ZUS1COLzt0mZwT2A9P/AwmSkWy+gh5HWVA5M2X1x66wkssR2x6H6lw==">
       <p>Symbol: <input type="text" name="symbol" id="symbol"></p>
