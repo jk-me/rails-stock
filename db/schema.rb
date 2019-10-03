0fa84a338cc5f2908ca8688ad173ac39c2be1cb6
Jenny Kam <jkam17@gmail.com>
2019-10-03 17:33:54 -0400

    remigrate databse with updated seed file, switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_30_223922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
