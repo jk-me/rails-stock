a17abf5057072812aafd0e7b87ac03739f2cfdf5
Jenny Kam <jkam17@gmail.com>
2019-10-04 16:45:39 -0400

    fix error display

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'accounts#new'
+  root 'accounts#show'
   get '/logout', to: 'sessions#logout', as: 'logout'
   post '/alphavantage', to: 'accounts#alphavantage', as: 'api'
-  post '/alpharesp', to: 'accounts#alpharesp', as: 'api_resp' 
+  post '/alpharesp', to: 'accounts#alpharesp', as: 'api_resp'
   get '/stocks', to: 'accounts#stocks', as: 'acc_stocks'
   get '/transactions', to: 'accounts#transactions', as: 'acc_trans'
 
