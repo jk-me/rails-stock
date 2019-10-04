9ef8491c1d9e2ccf91e2e4404aae3ffb690558f6
Jenny Kam <jkam17@gmail.com>
2019-10-04 01:59:57 -0400

    clean up js fns, add bootstrap and fix login/register styling

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'faraday'
 gem 'jquery-rails'
+gem 'bootstrap-sass', '~> 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -8,27 +8,7 @@ $(function(){
   // showPortfolio()
 })
 
-function updateVal(){
 
-}
-
-function showTransactions(){ //extra
-  $('.show-t').on('click', function(e){
-    e.preventDefault()
-    $('.page-name').html("<span class='page-title'>Transactions</span>")
-
-    $.get('/transactions', function(json){
-      let pagehtml = '<div>'
-      for (const el of json){
-        pagehtml += `
-          <p> BUY (${el.symbol}) - ${el.shares} @ ${el.price} ---- ${el.created_at}</p>
-           `
-      }
-      pagehtml+='</div>'
-      $('.container').html(pagehtml)
-    })
-  })
-}
 
 function updateVal(){
   $('.update').on('click', function(e){
@@ -42,14 +22,17 @@ function updateVal(){
           console.log(apijson)
           let val = parseFloat(apijson['Global Quote']['05. price']) * parseFloat(stock.shares)
           portfolioVal += val
-          $(`.${stock.id}`).text(`${stock.symbol}, ${stock.shares} shares -------- ${val}`)
-          $('#port-value').text(`($${portfolioVal})`)
+          $(`.${stock.id}`).text(`${stock.symbol}, ${stock.shares} shares -------- ${val.toFixed(2)}`)
+          $('#port-value').text(`($${portfolioVal.toFixed(2)})`)
 
           if (apijson['Global Quote']['05. price'] < apijson['Global Quote']['02. open']){
-            $(`.${stock.id}-color`)[0].style.backgroundColor = '#f6715f'
+            $(`.${stock.id}-color`)[0].style.backgroundColor = '#f6715f' //red, stock below open price
+          }
+          else if (apijson['Global Quote']['05. price'] === apijson['Global Quote']['02. open']){
+            $(`.${stock.id}-color`)[0].style.backgroundColor = '#acaeb9' //grey, at open price
           }
           else{
-            $(`.${stock.id}-color`)[0].style.backgroundColor = '#adf759'
+            $(`.${stock.id}-color`)[0].style.backgroundColor = '#adf759'  //green, above open price
           }
 
           // console.log(apijson['Global Quote']['05. price'])
@@ -59,4 +42,20 @@ function updateVal(){
     })
   })
 
+}
+
+function showTransactions(){ //extra js function to load transactions without reloading page
+  $('.show-t').on('click', function(e){
+    e.preventDefault()
+    $('.page-name').html("<span class='page-title'>Transactions</span>")
+
+    $.get('/transactions', function(json){
+      let pagehtml = '<div>'
+      for (const el of json){
+        pagehtml += `<p> BUY (${el.symbol}) - ${el.shares} @ ${el.price} ---- ${el.created_at}</p>`
+      }
+      pagehtml+='</div>'
+      $('.container').html(pagehtml)
+    })
+  })
 }
